d6a24bddddbd10c850aeae10ba6934e43e182df6
Bob Recny <bob.recny@u-blox.com>
2021-08-03 10:12:29 -0700

    Added u-blox copyrights
    Signed-off-by: Bob Recny <bob.recny@u-blox.com>

--- zephyr/boards/arm/ubx_mininorab1_nrf5340/pre_dt_board.cmake
+++ zephyr/boards/arm/ubx_mininorab1_nrf5340/pre_dt_board.cmake
@@ -1,4 +1,5 @@
-# Copyright (c) 2021 Linaro Limited
+# u-blox MINI-NORA-B1 board configuration
+# Copyright (c) 2021 u-blox AG
 # SPDX-License-Identifier: Apache-2.0
 
 # Suppress "unique_unit_address_if_enabled" to handle the following overlaps:
