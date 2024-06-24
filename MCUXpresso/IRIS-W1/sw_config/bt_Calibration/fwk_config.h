b63e0610deca0ccff5ce8eb915ea1cdb10dcd5ca
tarangpatelublox <tarang.patel@u-blox.com>
2024-06-24 11:23:24 +0200

    add: bt and wifi supported files

---
+++ MCUXpresso/IRIS-W1/sw_config/bt_Calibration/fwk_config.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2022-2023 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef _FWK_CONFIG_H_
+#define _FWK_CONFIG_H_
+
+#include "fwk_platform_definitions.h"
+
+#ifndef gPlatformUseHwParameter_d
+#define gPlatformUseHwParameter_d 0
+#endif
+
+#ifndef gPlatformDisableBleLowPower_d
+#define gPlatformDisableBleLowPower_d 0
+#endif
+
+#ifndef gPlatformDisableSetBtCalData_d
+#define gPlatformDisableSetBtCalData_d 0
+#endif
+
+#ifndef gPlatformDisableSetBtCalDataAnnex100_d
+#define gPlatformDisableSetBtCalDataAnnex100_d 0
+#endif
+
+#ifndef gPlatformEnableTxPowerChangeWithCountry_d
+#define gPlatformEnableTxPowerChangeWithCountry_d 0
+#endif
+
+/*
+ * gPlatformSetAntDiversity_d
+ * value is 0, enable ant1(share antenna with annex100),or enable ant2 with external FEM(ble only case)
+ * value is 1, enable ant3(diversity with annex100)
+ * value is 2, enable ant4(diversity with annex100)
+ */
+#ifndef gPlatformSetAntDiversity_d
+#define gPlatformSetAntDiversity_d 0
+#endif
+
+#endif /* _FWK_CONFIG_H_ */
